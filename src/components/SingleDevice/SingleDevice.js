ea04adbb9d965b9bfde70bf76767a0a2a75db6dc
Sahar Cohen <saharcohen456@gmail.com>
2024-08-11 09:31:27 +0300

    moved to redux application state

--- src/components/SingleDevice/SingleDevice.js
+++ src/components/SingleDevice/SingleDevice.js
@@ -5,15 +5,8 @@ import SingleDeviceGame from "../SingleDeviceGame/SingleDeviceGame";
 import "./SingleDevice.css";
 
 function SingleDevice() {
-  const [spies, setSpies] = useState(2);
   const [addedPlaces, setAddedPlaces] = useState([]);
   const [isSettingsOpen, setIsSettingsOpen] = useState(true);
-
-  const updateSpies = (chg) => {
-    if (spies + chg === 0 || spies + chg === 17) return;
-    // if (spies + chg > players) return;
-    setSpies((prev) => prev + chg);
-  };
 
   const startGame = () => {
     setIsSettingsOpen(false);
